b383f7a6bb308021c70dc9a558c5d56584c719d6
Nandakishore N M <nmnandakishore@gmail.com>
2021-12-13 08:57:35 +0530

    Small screen bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 var path = require("path");
+var glob = require("glob");
 
 export default {
 	build: {
 		rollupOptions: {
 			input: {
 				main: path.resolve(__dirname, "index.html"),
+				about: path.resolve(__dirname, "about.html"),
+				leadership: path.resolve(__dirname, "leadership.html"),
+				sakarSpark: path.resolve(__dirname, "sakar-spark.html"),
 				// about: path.resolve(__dirname, "about.html"),
 				// projects: path.resolve(__dirname, "projects.html"),
 				// contact: path.resolve(__dirname, "contact.html"),
